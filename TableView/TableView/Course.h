ea93abc025b939b83133f2baa2db50db10f7c5be
JoeSuzuki <joesuzuki516@gmail.com>
2018-04-11 18:51:39 -0400

    new @property for cells

---
+++ TableView/TableView/Course.h
@@ -0,0 +1,16 @@
+//
+//  Course.h
+//  TableView
+//
+//  Created by Joe Suzuki on 4/11/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Course : NSObject
+
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSNumber *numberOfLessons;
+
+@end
---
+++ TableView/TableView/Course.m
@@ -0,0 +1,13 @@
+//
+//  Course.m
+//  TableView
+//
+//  Created by Joe Suzuki on 4/11/18.
+//  Copyright © 2018 Joe Suzuki. All rights reserved.
+//
+
+#import "Course.h"
+
+@implementation Course
+
+@end
