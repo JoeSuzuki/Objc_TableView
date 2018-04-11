b0e884456cb37220aabe10e3d64d2a6845f25487
JoeSuzuki <joesuzuki516@gmail.com>
2018-04-10 23:38:33 -0400

    tableview methods override

--- TableView/TableView/ViewController.m
+++ TableView/TableView/ViewController.m
@@ -21,5 +21,13 @@
     self.navigationController.navigationBar.prefersLargeTitles = YES;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellId" forIndexPath:indexPath];
+    return cell;
+}
 
 @end
